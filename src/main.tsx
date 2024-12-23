00cf27741cbf61cd33324d8f88a400b2e84e4f0a
Alexander Pavlov <xanbimei@ya.ru>
2024-12-23 04:09:52 +0300

    remove gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import store from './app/store.ts';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
