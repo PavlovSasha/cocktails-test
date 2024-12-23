1553701888f566040269fa5bbb5cc399a3bd8ce0
Alexander Pavlov <xanbimei@ya.ru>
2024-12-23 03:51:47 +0300

    vite-plugin-gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import store from './app/store.ts';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </Provider>
